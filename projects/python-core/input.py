d08a34a0e5fffd5e6f674d284f39d5529222dcc1
Anh Thach <anhthach291@gmail.com>
2025-10-14 18:04:26 +0700

    update script

--- projects/python-core/input.py
+++ projects/python-core/input.py
@@ -1,4 +1,18 @@
 # Import necessary modules
 
 # Define class
-class 
+class Vehicle:
+    def __init__(self, id, name, color, price, brand, type, yearOfManufacture):
+        self.id = id
+        self.name = name
+        self.color = color
+        self.price = price
+        self.brand = brand
+    
+    @staticmethod
+    def display_vehicle_info(vehicle):
+        print(f"Vehicle ID: {vehicle.id}")
+        print(f"Name: {vehicle.name}")
+        print(f"Color: {vehicle.color}")
+        print(f"Price: {vehicle.price}")
+        print(f"Brand: {vehicle.brand}")
